3761260e6c3df843c7ab55e2c5828ebd42180f8e
Jeremy Zongker <jeremy@trilitech.com>
2022-11-10 10:53:39 -0600

    Removed appBase (should be submodule)

--- src/appBase/donationComponents/components/index.tsx
+++
@@ -1,9 +0,0 @@
-export { BankForm } from "./BankForm";
-export { CardForm } from "./CardForm";
-export { DonationForm } from "./DonationForm";
-export { OneTimeDonation } from "./OneTimeDonation";
-export { PaymentMethods } from "./PaymentMethods";
-export { RecurringDonations } from "./RecurringDonations";
-export { RecurringDonationsEdit } from "./RecurringDonationsEdit";
-export { FundDonations } from "./FundDonations";
-export { FundDonation } from "./FundDonation";
