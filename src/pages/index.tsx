e10695fc36a8fd239e70c04094de221883ddffa8
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-11-16 00:59:51 +0530

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import { GetStaticProps } from "next";
 import Link from "next/link";
+import { YoutubeBackground } from "components/YoutubeBackground";
 
 export default function Home() {
-  return (<>
-    <p>Select a site:</p>
-    <Link href="http://crcc.localhost:3000/test">CRCC</Link><br />
-    <Link href="http://crcc.localhost:3000/test">Ironwood</Link>
-  </>);
+  return (
+    <YoutubeBackground videoId="3iXYciBTQ0c" overlay="rgba(0,0,0,.4)">
+      <p>Select a site:</p>
+      <Link href="http://crcc.localhost:3000/test">CRCC</Link><br />
+      <Link href="http://crcc.localhost:3000/test">Ironwood</Link>
+    </YoutubeBackground>
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-
   return {
     props: {},
     revalidate: 30,
