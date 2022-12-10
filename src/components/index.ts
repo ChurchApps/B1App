0ffe0b599d9f48ac2ee037a49516e435348a185a
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-12-10 09:59:51 +0530

    use markdown from appbase

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { ErrorMessages } from "@/appBase/components/ErrorMessages";
 export { DisplayBox } from "@/appBase/components/DisplayBox";
 export { Loading } from "@/appBase/components/Loading";
 export { InputBox } from "@/appBase/components/InputBox";
-export { LogoutPage } from "@/appBase/pageComponents/LogoutPage"
+export { LogoutPage } from "@/appBase/pageComponents/LogoutPage";
+export { Markdown } from "@/appBase/components/markdown";
