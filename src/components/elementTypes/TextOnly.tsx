d9e72e22645ab9ff10682843639da273d0b9cd45
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-12-14 10:36:58 +0530

    fix markdownc omponent

--- src/components/elementTypes/TextOnly.tsx
+++ src/components/elementTypes/TextOnly.tsx
@@ -1,9 +1,12 @@
+import dynamic from "next/dynamic";
 import { ElementInterface } from "@/helpers";
 import { MarkdownPreview } from "@/components"
 
 interface Props { element: ElementInterface }
 
+const Preview = dynamic(() => import("@uiw/react-markdown-preview"), { ssr: false });
+
 export const TextOnly: React.FC<Props> = props => {
-  let result = <MarkdownPreview value={props.element.answers?.text || ""} />
+  let result = <MarkdownPreview editor={Preview} value={props.element.answers?.text || ""} />
   return result;
 }
--- src/components/elementTypes/TextWithPhoto.tsx
+++ src/components/elementTypes/TextWithPhoto.tsx
@@ -1,11 +1,14 @@
+import dynamic from "next/dynamic";
 import { ElementInterface } from "@/helpers";
 import { Grid } from "@mui/material";
 import { MarkdownPreview } from "@/components";
 
 interface Props { element: ElementInterface }
 
+const Preview = dynamic(() => import("@uiw/react-markdown-preview"), { ssr: false });
+
 export const TextWithPhoto: React.FC<Props> = props => {
-  let result = <MarkdownPreview value={props.element.answers?.text || ""} />
+  let result = <MarkdownPreview editor={Preview} value={props.element.answers?.text || ""} />
   switch (props.element.answers?.photoPosition || "left") {
     case "left":
       result = (
@@ -14,7 +17,7 @@ export const TextWithPhoto: React.FC<Props> = props => {
             <img src={props.element.answers?.photo || "about:blank"} alt={props.element.answers?.photoAlt || ""} style={{ borderRadius: 10, marginTop: 40 }} />
           </Grid>
           <Grid item md={8} xs={12}>
-            <MarkdownPreview value={props.element.answers?.text || ""} />
+            <MarkdownPreview editor={Preview} value={props.element.answers?.text || ""} />
           </Grid>
         </Grid>
       )
@@ -23,7 +26,7 @@ export const TextWithPhoto: React.FC<Props> = props => {
       result = (
         <Grid container columnSpacing={3}>
           <Grid item md={8} xs={12}>
-            <MarkdownPreview value={props.element.answers?.text || ""} />
+            <MarkdownPreview editor={Preview} value={props.element.answers?.text || ""} />
           </Grid>
           <Grid item md={4} xs={12}>
             <img src={props.element.answers?.photo || "about:blank"} alt={props.element.answers?.photoAlt || ""} style={{ borderRadius: 10, marginTop: 40 }} />
@@ -34,7 +37,7 @@ export const TextWithPhoto: React.FC<Props> = props => {
     case "bottom":
       result = (
         <>
-          <MarkdownPreview value={props.element.answers?.text || ""} />
+          <MarkdownPreview editor={Preview} value={props.element.answers?.text || ""} />
           <img src={props.element.answers?.photo || "about:blank"} alt={props.element.answers?.photoAlt || ""} style={{ borderRadius: 10, marginTop: 40 }} />
         </>
       )
@@ -43,7 +46,7 @@ export const TextWithPhoto: React.FC<Props> = props => {
       result = (
         <>
           <img src={props.element.answers?.photo || "about:blank"} alt={props.element.answers?.photoAlt || ""} style={{ borderRadius: 10, marginTop: 40 }} />
-          <MarkdownPreview value={props.element.answers?.text || ""} />
+          <MarkdownPreview editor={Preview} value={props.element.answers?.text || ""} />
         </>
       )
       break;
