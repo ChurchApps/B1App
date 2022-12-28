8eb04091814e338041b7dfc18bc6b5631a853403
Pranav Malvawala <pranavmalvawala@gmail.com>
2022-12-28 17:44:23 +0530

    update editor

--- src/components/elementTypes/TextOnly.tsx
+++ src/components/elementTypes/TextOnly.tsx
@@ -1,4 +1,3 @@
-import dynamic from "next/dynamic";
 import { ElementInterface } from "@/helpers";
 import { MarkdownPreview } from "@/components";
 
@@ -6,9 +5,7 @@ interface Props {
   element: ElementInterface;
 }
 
-const Editor = dynamic(() => import("react-draft-wysiwyg").then((mod) => mod.Editor), { ssr: false });
-
 export const TextOnly: React.FC<Props> = (props) => {
-  let result = <MarkdownPreview editor={Editor} value={props.element.answers?.text || ""} />;
+  let result = <MarkdownPreview value={props.element.answers?.text || ""} />;
   return result;
 };
