204f45c3951eabaee6d99c6350dc1b702f387056
Jeremy Zongker <jeremy@trilitech.com>
2023-01-19 17:03:34 -0600

    Implemented home page route

--- src/pages/[sdSlug]/index.tsx
+++ src/pages/[sdSlug]/index.tsx
@@ -1,8 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import { Layout, Loading } from "@/components";
-import { Section } from "@/components/Section";
-import pageData from "../../samplePages/newhere.json";
-import { ApiHelper, ChurchInterface, EnvironmentHelper, LinkInterface } from "@/helpers";
+import { Loading, PageLayout } from "@/components";
+import { ApiHelper, ChurchInterface, EnvironmentHelper, LinkInterface, PageInterface } from "@/helpers";
 import { useEffect } from "react";
 
 type Props = {
@@ -17,24 +15,8 @@ export default function Home(props: Props) {
     if (EnvironmentHelper.HideYoursite && typeof window !== undefined) window.location.href = window.location.origin + "/member";
   }, []); //eslint-disable-line
 
-  const getSections = () => {
-    const result: JSX.Element[] = []
-    let first = true;
-    for (let section of props.pageData.sections) {
-      result.push(<Section key={section.id} section={section} first={first} />)
-      first = false;
-    }
-    return result;
-  }
-
   if (EnvironmentHelper.HideYoursite) return <Loading />
-  else return (
-    <Layout church={props.church} churchSettings={props.churchSettings} navLinks={props.navLinks}>
-      <div className="page">
-        {getSections()}
-      </div>
-    </Layout>
-  );
+  else return (<PageLayout church={props.church} churchSettings={props.churchSettings} navLinks={props.navLinks} pageData={props.pageData} />);
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -52,8 +34,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const churchSettings: any = await ApiHelper.getAnonymous("/settings/public/" + church.id, "MembershipApi");
   const navLinks: any = await ApiHelper.getAnonymous("/links/church/" + church.id + "?category=website", "ContentApi");
 
-  //const pageData: PageInterface = await fetch("http://localhost:3000/samplePages/about.json").then(resp => resp.json());
-  //const pageData: PageInterface = await fetch("http://localhost:3000/samplePages/newhere.json").then(resp => resp.json());
+  const pageData: PageInterface = await ApiHelper.getAnonymous("/pages/" + church.id + "/tree?url=/", "ContentApi");
 
   return {
     props: { pageData, church, churchSettings, navLinks },
