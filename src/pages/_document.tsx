af4c937cdd8be19bc9def372cf59d1a25bc81385
Jeremy Zongker <jeremy@zongker.net>
2023-02-22 19:20:51 -0600

    Working on appearance

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <link rel="stylesheet" href="/css/buttons.css" />
         </Head>
         <body>
           <Main />
