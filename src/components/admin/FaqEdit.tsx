7da208923053c2a4a47f23cb2bf431e18024ace4
Himali Malvawala <himalimalvawala@gmail.com>
2023-03-11 17:08:42 +0530

    feat: allow to add FAQ-accordion

--- src/components/admin/FaqEdit.tsx
+++ src/components/admin/FaqEdit.tsx
@@ -1,18 +1,4 @@
-import { useState } from "react";
-import {
-  TextField,
-  Box,
-  SelectChangeEvent,
-  Stack,
-  Button,
-  InputLabel,
-  Table,
-  TableBody,
-  TableRow,
-  TableCell,
-  TableHead,
-  Typography,
-} from "@mui/material";
+import { TextField, Box, SelectChangeEvent } from "@mui/material";
 import { MarkdownEditor } from "..";
 
 type Props = {
@@ -21,90 +7,23 @@ type Props = {
   handleMarkdownChange: (field: string, newValue: string) => void;
 };
 
-export const FaqEdit = ({
-  parsedData,
-  handleChange,
-  handleMarkdownChange,
-}: Props) => {
-  const [elements, setElements] = useState([]);
-  const [faqTitle, setFaqTitle] = useState("");
-  const [faqDescription, setFaqDescription] = useState("");
-
-  const changeHandler = () => {}
-
-  const addElement = () => {
-    console.log("add got clicked!");
-    const obj = { title: parsedData.faqTitle, description: parsedData.text };
-    const newArray = [...elements];
-    newArray.push(obj);
-    setElements(newArray);
-  };
-
-  // if (elements.length !== 0) {
-  //   parsedData["lists"] = elements;
-  // }
-  // elements.forEach((e) => {
-  //   console.log("eeeeeeeeeeeeeeeee: ", e);
-
-  //   parsedData[`accordion_${e.title}`] = e.description;
-  // });
-  // console.log("elements: ", elements);
-
-  // console.log("parsed data: ", parsedData);
-
-  const getRows = () => {
-    return (
-      <Table size="small">
-        <TableHead>
-          <TableRow sx={{ textAlign: "left" }}>
-            <th>Value</th>
-            <th>Action</th>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {parsedData?.list?.map((element) => (
-            <TableRow>
-              <TableCell>
-                <Typography>{element.title}</Typography>
-              </TableCell>
-              <TableCell>
-                <Button>Remove</Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    );
-  };
-
+export const FaqEdit = ({ parsedData, handleChange, handleMarkdownChange }: Props) => {
   return (
     <>
-    <TextField fullWidth label="Title" name="faqTitle" value={faqTitle} onChange={changeHandler} />
-    <Box sx={{ marginTop: 2 }}>
-      <MarkdownEditor
-      value={faqDescription}
-      onChange={changeHandler}
-      style={{ maxHeight: 200, overflowY: "scroll" }}
-      />
-    </Box>
-    {/* {getRows()}
       <TextField
         fullWidth
         label="Title"
-        name="faqTitle"
-        value={parsedData.faqTitle || ""}
+        name="title"
+        value={parsedData.title || ""}
         onChange={handleChange}
       />
       <Box sx={{ marginTop: 2 }}>
         <MarkdownEditor
-          value={parsedData.text || ""}
-          onChange={(val) => handleMarkdownChange("text", val)}
+          value={parsedData.description || ""}
+          onChange={(val) => handleMarkdownChange("description", val)}
           style={{ maxHeight: 200, overflowY: "scroll" }}
         />
       </Box>
-        <Button sx={{marginTop: 2}} fullWidth variant="contained" onClick={addElement}>
-          Add
-        </Button> */}
     </>
   );
 };
--- src/components/elementTypes/FaqElement.tsx
+++ src/components/elementTypes/FaqElement.tsx
@@ -1,10 +1,9 @@
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import MuiAccordionSummary, { AccordionSummaryProps } from "@mui/material/AccordionSummary";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 import { ElementInterface } from "@/helpers";
 import { MarkdownPreview } from "..";
 
@@ -12,19 +11,51 @@ interface Props {
   element: ElementInterface;
 }
 
-export const FaqElement = ({ element }: Props) => {
-  // console.log("ELEMENT: ", element);
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  background: "transparent",
+  "&:before": {
+    display: "none",
+  },
+}));
 
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<DoubleArrowIcon sx={{ color: "#03a9f4" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  flexDirection: "row-reverse",
+  backgroundColor: "transparent",
+  marginTop: 10,
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+  "& .MuiAccordionSummary-content.Mui-expanded": {
+    marginLeft: theme.spacing(1),
+    // color: "#00000080",
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  marginLeft: theme.spacing(1),
+}));
+
+export const FaqElement = ({ element }: Props) => {
   return (
     <>
-      <Accordion style={{ borderRadius: 3 }}>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>{element.answers.faqTitle}</Typography>
+      <Accordion>
+        <AccordionSummary>
+          <Typography variant="h6" fontWeight={600}>
+            {element?.answers?.title}
+          </Typography>
         </AccordionSummary>
-        <AccordionDetails
-          style={{ backgroundColor: "#eeeeee", borderRadius: 3 }}
-        >
-          <MarkdownPreview value={element.answers.text || ""} />
+        <AccordionDetails>
+          <MarkdownPreview value={element?.answers?.description} />
         </AccordionDetails>
       </Accordion>
     </>
