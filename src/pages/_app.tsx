0994661f80e8cf9f6c96587ff1f0c2f45ba3ff15
Himali Malvawala <himalimalvawala@gmail.com>
2023-04-15 14:26:04 +0530

    improve

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,10 @@ import "@/styles/streaming.css";
 import "@/styles/buttons.css";
 import "@/appBase/components/markdownEditor/editor.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Head from "next/head";
-import { EnvironmentHelper, AppearanceHelper } from "@/helpers";
+import { EnvironmentHelper, AppearanceHelper, ConfigHelper } from "@/helpers";
+import { ConfigurationInterface } from "@/helpers/ConfigHelper";
 import { UserProvider } from "@/context/UserContext";
 import { AnalyticsHelper } from "@/appBase/helpers";
 import React from "react";
@@ -14,11 +16,23 @@ import React from "react";
 EnvironmentHelper.init();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [config, setConfig] = React.useState<ConfigurationInterface>(null);
+  const router = useRouter();
+  
   const location = (typeof(window) === "undefined") ? null : window.location;
   AnalyticsHelper.init();
   React.useEffect(() => { AnalyticsHelper.logPageView() }, [location]);
 
-  const favicon = pageProps?.config?.appearance?.favicon_16x16 && AppearanceHelper.getFavicon(pageProps?.config?.appearance, "16");
+  React.useEffect(() => {
+    const getConfig = async () => {
+      const response = await ConfigHelper.load(router.query?.sdSlug?.toString());
+      response && setConfig(response);
+      return response;
+    }
+    getConfig();
+  }, [])
+
+  const favicon = config?.appearance?.favicon_16x16 && AppearanceHelper.getFavicon(config.appearance, "16");
 
   return (
     <UserProvider>
