d37ad88b7ef3755009eb429b59d10909ed9fd8e5
Himali Malvawala <himalimalvawala@gmail.com>
2023-05-14 23:55:10 +0530

    fix: events visibility for weeks/days

--- src/components/eventCalendar/EventCalendar.tsx
+++ src/components/eventCalendar/EventCalendar.tsx
@@ -50,6 +50,10 @@ export function EventCalendar(props:Props) {
   endRange.setFullYear(endRange.getFullYear() + 1);
 
   props.events.forEach((event) => {
+    const ev = {...event};
+    // TODO: check for TimeZone.
+    ev.start = new Date(ev.start);
+    ev.end = new Date(ev.end);
     if (event.recurrenceRule) {
       const dates = EventHelper.getRange(event, startRange, endRange);
       dates.forEach((date) => {
@@ -61,7 +65,7 @@ export function EventCalendar(props:Props) {
       });
       EventHelper.removeExcludeDates(expandedEvents);
     }
-    else expandedEvents.push(event);
+    else expandedEvents.push(ev);
   });
 
   return (
