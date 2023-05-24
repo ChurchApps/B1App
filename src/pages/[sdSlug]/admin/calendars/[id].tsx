bb849b78269817b0d3f9f54aa09d95b76f030e2b
Himali Malvawala <himalimalvawala@gmail.com>
2023-05-24 22:00:08 +0530

    feat: add group to curated calendars

--- src/pages/[sdSlug]/admin/calendars/[id].tsx
+++ src/pages/[sdSlug]/admin/calendars/[id].tsx
@@ -1,25 +1,107 @@
 import { useEffect, useState } from "react";
 import { GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router";
-import { ConfigHelper, ApiHelper, WrapperPageProps, CuratedCalendarInterface } from "@/helpers";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Box, Typography, Button, Dialog, DialogTitle, DialogContent, DialogActions, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import { ConfigHelper, ApiHelper, WrapperPageProps, CuratedCalendarInterface, CuratedEventInterface, GroupInterface, EventInterface } from "@/helpers";
 import { AdminWrapper } from "@/components/admin/AdminWrapper";
+import { DisplayBox, Loading } from "@/components";
 
 export default function CalendarPage(props: WrapperPageProps) {
   const { isAuthenticated } = ApiHelper;
+
   const [currentCalendar, setCurrentCalendar] = useState<CuratedCalendarInterface>(null);
+  const [open, setOpen] = useState<boolean>(false);
+  const [groups, setGroups] = useState<GroupInterface[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [selectedGroupId, setSelectedGroupId] = useState<string>("");
+  const [curatedEvents, setCuratedEvents] = useState<CuratedEventInterface[]>([]);
+  const [events, setEvents] = useState<EventInterface[]>([]);
+
   const router = useRouter();
   const curatedCalendarId = router.query?.id;
+  const localizer = momentLocalizer(moment);
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down(396));
 
   const loadData = () => {
     if (!isAuthenticated) router.push("/login");
     ApiHelper.get("/curatedCalendars/" + curatedCalendarId, "ContentApi").then((data) => setCurrentCalendar(data));
   };
 
-  useEffect(() => { loadData(); }, []);
+  const loadGroups = () => {
+    setIsLoading(true);
+    ApiHelper.get("/groups/my", "MembershipApi").then((data) => { setGroups(data); setIsLoading(false); });
+  };
+  
+  const loadCuratedEvents = () => {
+    ApiHelper.get("/curatedEvents/calendar/" + curatedCalendarId, "ContentApi").then((data) => setCuratedEvents(data));
+  };
+  
+  const loadEvents = () => {
+    ApiHelper.get("/curatedEvents/calendar/" + curatedCalendarId + "/events", "ContentApi").then((data) => setEvents(data));
+  };
+  
+  const handleSave = () => {
+    const data = { curatedCalendarId: curatedCalendarId, groupId: selectedGroupId as string };
+    ApiHelper.post("/curatedEvents", [data], "ContentApi").then(() => {
+      setSelectedGroupId("");
+      setOpen(false);
+      loadCuratedEvents();
+      loadEvents();
+    });
+  };
+  
+  const filteredGroups = groups.filter((g) => {
+    return !curatedEvents.find((crtEv) => {
+      return crtEv.groupId === g.id;
+    });
+  });
 
+  useEffect(() => { loadData(); loadCuratedEvents(); loadEvents(); }, []);
+  
   return (
     <AdminWrapper config={props.config}>
       <h1>{currentCalendar?.name}</h1>
+      <DisplayBox headerText="">
+        <Box
+          sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", marginBottom: 3 }}>
+          <Typography component="h2" variant="h6" color="primary">Curated Calendar</Typography>
+          <Button endIcon={<EventNoteIcon />} size="small" variant="contained" onClick={() => { setOpen(true); loadGroups(); }}>Add</Button>
+        </Box>
+        <Calendar localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 500 }} />
+      </DisplayBox>
+      <Dialog open={open} onClose={() => { setOpen(false); setSelectedGroupId(""); }} fullWidth scroll="body" fullScreen={fullScreen}>
+        <DialogTitle>Add a Group</DialogTitle>
+        <DialogContent>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <>
+              {filteredGroups?.length > 0 ? (
+                <FormControl fullWidth>
+                  <InputLabel>Select a Group</InputLabel>
+                  <Select fullWidth label="Select a Group" value={selectedGroupId} onChange={(e: SelectChangeEvent) => setSelectedGroupId(e.target.value as string)}>
+                    {filteredGroups.map((group) => <MenuItem key={group.id} value={group.id}>{group.name}</MenuItem>)}
+                  </Select>
+                </FormControl>
+              ) : (
+                <Typography>You've added all the available groups.</Typography>
+              )}
+            </>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button variant="text" onClick={() => { setOpen(false); setSelectedGroupId(""); }}>cancel</Button>
+          <Button variant="contained" onClick={handleSave} disabled={!selectedGroupId}>Save</Button>
+        </DialogActions>
+      </Dialog>
     </AdminWrapper>
   );
 }
