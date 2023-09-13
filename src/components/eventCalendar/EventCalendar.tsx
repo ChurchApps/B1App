82a6c0d36ce192a39bd5f3949907008d30b01059
Jeremy Zongker <jeremy@zongker.net>
2023-09-13 14:10:34 -0500

    Fixed add event formatting

--- src/components/eventCalendar/EventCalendar.tsx
+++ src/components/eventCalendar/EventCalendar.tsx
@@ -20,8 +20,12 @@ export function EventCalendar(props:Props) {
   const handleAddEvent = (slotInfo: any) => {
     const startTime = new Date(slotInfo.start);
     startTime.setHours(12);
+    startTime.setMinutes(0);
+    startTime.setSeconds(0);
     const endTime = new Date(slotInfo.start);
     endTime.setHours(13);
+    startTime.setMinutes(0);
+    startTime.setSeconds(0);
     setEditEvent({ start: startTime, end: endTime, allDay:false, groupId: props.editGroupId, visibility: "public" })
   }
 
@@ -68,7 +72,7 @@ export function EventCalendar(props:Props) {
 
   return (
     <div>
-      {props.editGroupId && <SmallButton icon="event_note" text="Add" onClick={() => { handleAddEvent({ start:new Date(), end: new Date() }) }} /> }
+      {props.editGroupId && <div style={{textAlign:"right", marginBottom:20}}><SmallButton icon="event_note" text="Add Event" onClick={() => { handleAddEvent({ start:new Date(), end: new Date() }) }} /></div> }
       <Calendar localizer={localizer} events={expandedEvents} startAccessor="start" endAccessor="end" style={{ height: 500 }} onSelectEvent={handleEventClick} onSelectSlot={handleAddEvent} selectable={props.editGroupId !== null} />
       {editEvent && props.editGroupId && <EditEventModal event={editEvent} onDone={ handleDone } />}
       {displayEvent && <DisplayEventModal event={displayEvent} onDone={ handleDone } canEdit={props.editGroupId!==""} onEdit={() => { setEditEvent(displayEvent); setDisplayEvent(null); }} />}
