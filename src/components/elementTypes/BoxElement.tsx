7c2bd493c9730d68f53db3285dc6ca0bb4025934
Himali Malvawala <himalimalvawala@gmail.com>
2024-01-03 22:45:55 +0530

    allow dragging elements to top/bottom in box

--- src/components/elementTypes/BoxElement.tsx
+++ src/components/elementTypes/BoxElement.tsx
@@ -2,16 +2,27 @@ import { ElementInterface, SectionInterface } from "@/helpers";
 import { DroppableArea } from "../admin/DroppableArea";
 import { Element } from "../Element";
 import { CSSProperties } from "react";
+import { ApiHelper } from "@churchapps/apphelper";
 import { StyleHelper } from "@/helpers/StyleHelper";
 
 interface Props { element: ElementInterface, churchSettings: any, textColor: string, onEdit?: (section: SectionInterface, element: ElementInterface) => void, onMove?: () => void }
 
 export function BoxElement(props: Props) {
 
+  const handleDrop = (data: any, sort: number) => {
+    if (data.data) {
+      const e: ElementInterface = data.data;
+      e.sort = sort;
+      ApiHelper.post("/elements", [e], "ContentApi").then(() => { props.onMove() });
+    } else {
+      const e: ElementInterface = { sectionId: props.element.sectionId, elementType: data.elementType, sort, parentId: props.element.id, blockId: props.element.blockId };
+      props.onEdit(null, e);
+    }
+  }
 
   const getAddElement = (s: number, droppableAreaText?: string) => {
     const sort = s;
-    return (<DroppableArea key={"addToBox"} accept={["element", "elementBlock"]} text={droppableAreaText} onDrop={(data) => props.onEdit(null, { sectionId: props.element.sectionId, elementType: data.elementType, sort, parentId: props.element.id, blockId: props.element.blockId })} dndDeps={props.element?.elements} />);
+    return (<DroppableArea key={"addToBox"} accept={["element", "elementBlock"]} text={droppableAreaText} onDrop={(data) => handleDrop(data, sort)} dndDeps={props.element?.elements} />);
   }
 
   const getElements = () => {
