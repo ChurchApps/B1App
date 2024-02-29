68d4bbed37113e2b620bac83a6a2471ff0c8c082
Jeremy Zongker <jeremy@zongker.net>
2024-02-29 14:25:39 -0600

    Improved appearance of portal home screen.

---
+++ src/components/member/timeline/MyGroups.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { Grid, Typography, Box } from "@mui/material";
+import { ApiHelper, GroupInterface } from "@churchapps/apphelper";
+
+export default function MyGroups() {
+  const [groups, setGroups] = useState<GroupInterface[]>([]);
+
+  const loadData = () => {
+    ApiHelper.get("/groups/my", "MembershipApi").then((data) => setGroups(data));
+  };
+
+  useEffect(loadData, []);
+
+  return (
+    <Grid container spacing={3}>
+      {groups?.length > 0
+        ? (
+          groups.map((group) => (
+            <Grid item xs={12}>
+              <Link href={"/member/groups/" + group.id}>
+                <Box
+                  id="tabImage"
+                  sx={{
+                    backgroundImage: `url(${group.photoUrl})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "100% 100%",
+                    backgroundColor: group.photoUrl ? "#616161" : "#000000",
+                    backgroundBlendMode: group.photoUrl ? "overlay" : "",
+                    aspectRatio: "4",
+                    width: "100%",
+                    color: "white",
+                    textAlign: "center",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                  }}
+                >
+                  <Typography noWrap sx={{ fontSize: { lg: 34, md: 26, xs: 24 }, color: "#FFFFFF", padding: 2 }} style={{ color: "#FFF" }}>
+                    {group.name}
+                  </Typography>
+                </Box>
+              </Link>
+            </Grid>
+          ))
+        )
+        : (
+          <p>No groups found</p>
+        )}
+    </Grid>
+  );
+}
--- src/pages/[sdSlug]/member/index.tsx
+++ src/pages/[sdSlug]/member/index.tsx
@@ -1,21 +1,38 @@
 import { Wrapper } from "@/components";
+import MyGroups from "@/components/member/timeline/MyGroups";
 import { Timeline } from "@/components/member/timeline/Timeline";
 import UserContext from "@/context/UserContext";
 import { ConfigHelper, WrapperPageProps } from "@/helpers";
+import { ApiHelper } from "@churchapps/apphelper";
 import { Grid } from "@mui/material";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Link from "next/link";
 import { useContext } from "react";
 
 export default function Member(props: WrapperPageProps) {
   const context = useContext(UserContext)
 
+  if (!ApiHelper.isAuthenticated) {
+    return (
+      <Wrapper config={props.config}>
+        <h1>Member Portal</h1>
+        <p>Select an option on the left or <Link href="/login/?returnUrl=/member/groups">Login</Link>.</p>
+      </Wrapper>
+    );
+  }
+
   return (
     <Wrapper config={props.config}>
-      <h1>Member Portal</h1>
-      <p>Select an option on the left</p>
+      <h1>Latest Updates</h1>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Timeline context={context} />
+        <Grid item xs={12} md={8}>
+          <div style={{maxWidth: 750, marginLeft:"auto", marginRight:"auto"}}>
+            <Timeline context={context} />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <MyGroups />
+
         </Grid>
       </Grid>
 
