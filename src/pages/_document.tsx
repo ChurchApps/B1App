63ce0ae4ac6c101f413de210b811c4faec4fb8d5
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2024-06-05 12:49:00 +0530

    chore: check og:image (#273)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <meta property="og:image" content="https://feather.so/images/og-image.png" />
         </Head>
         <body>
           <Main />
