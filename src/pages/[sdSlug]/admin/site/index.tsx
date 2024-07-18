55c816d2f89ca4829b52d294f50a4c09c3a472b8
Jeremy Zongker <jeremy@zongker.net>
2024-07-18 12:45:55 -0500

    Cleaned up /admin/site page

--- src/pages/[sdSlug]/admin/site/index.tsx
+++ src/pages/[sdSlug]/admin/site/index.tsx
@@ -1,99 +1,91 @@
 import { useEffect, useState } from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
 import router from "next/router";
-import { ConfigHelper, PageInterface, WrapperPageProps } from "@/helpers";
-import { ApiHelper, DisplayBox, ErrorMessages, LinkInterface, SmallButton } from "@churchapps/apphelper";
-import { Grid, TableCell, TableRow } from "@mui/material";
+import { BlockInterface, ConfigHelper, WrapperPageProps } from "@/helpers";
+import { ApiHelper, DisplayBox, ErrorMessages, SmallButton } from "@churchapps/apphelper";
+import { Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import { useWindowWidth } from "@react-hook/window-size";
-import { SiteNavigation } from "@/components/admin/SiteNavigation";
-import { NavItemEdit } from "@/components/admin/site/NavItemEdit";
 import { AdminSiteWrapper } from "@/components/admin/AdminSiteWrapper";
+import { BlockEdit } from "@/components/admin/BlockEdit";
+import Link from "next/link";
 
 export default function Admin(props: WrapperPageProps) {
   const { isAuthenticated } = ApiHelper;
-  const [links, setLinks] = useState<LinkInterface[]>([]);
-  const [pages, setPages] = useState<PageInterface[]>([]);
+  const [blocks, setBlocks] = useState<BlockInterface[]>([]);
+  const [editBlock, setEditBlock] = useState<BlockInterface>(null);
   const [errors, setErrors] = useState<string[]>([]);
   const windowWidth = useWindowWidth();
 
   const loadData = () => {
     if (!isAuthenticated) return;
 
-    ApiHelper.get("/pages", "ContentApi").then((_pages) => {
-      let filteredPages:PageInterface[] = [];
-      _pages.forEach((p:PageInterface) => {
-        if (!p.url.startsWith("/stream/") && !p.url.startsWith("/member/")) filteredPages.push(p);
-      });
-      setPages(filteredPages || [])
-    });
-
-    ApiHelper.get("/links?category=website", "ContentApi").then(data => { setLinks(data); });
+    ApiHelper.get("/blocks", "ContentApi").then((b) => setBlocks(b || []));
   };
+
+  const editBlockContent = (
+    <SmallButton
+      icon="add"
+      onClick={() => {
+        setEditBlock({ blockType: "elementBlock" });
+      }}
+    />
+  );
+
+  const blocksUi = blocks.map((block) => (
+    <TableRow key={block.id}>
+      <TableCell>
+        <Link href={"/admin/site/blocks/" + block.id} style={{cursor: "pointer"}}>{block.name}</Link>
+      </TableCell>
+      <TableCell>{block.blockType === "elementBlock" ? "Element(s)" : "Section(s)"}</TableCell>
+      <TableCell align="right">
+        <SmallButton
+          icon="edit"
+          onClick={() => {
+            setEditBlock(block);
+          }}
+        />
+      </TableCell>
+    </TableRow>
+  ));
 
   useEffect(() => { if (!isAuthenticated) router.push("/login"); }, []);
   useEffect(loadData, [isAuthenticated]);
 
-  const navigationHandler = (errorMessage: string, url: string) => {
-    let errors: string[] = [];
-
-    if (windowWidth > 882){
-      router.push(url);
-    } else {
-      errors.push(errorMessage);
-    }
-
-    if (errors.length > 0) {
-      setErrors(errors);
-      return;
-    }
-  }
-
-  const getUnlinkedPages = () => {
-    const unlinkedPages = pages.filter(p => !links.find(l => l.url === p.url));
-    const result = unlinkedPages.map((page) => {
-
-      const clickHandler = () => navigationHandler("Page editor is only accessible on desktop", "/admin/site/pages/" + page.id);
-
-      return (
-        <TableRow key={page.id}>
-          <TableCell>
-            <a onClick={clickHandler} style={{cursor: "pointer"}}>{page.url}</a>
-          </TableCell>
-          <TableCell>
-            <a onClick={clickHandler} style={{cursor: "pointer"}}>{page.title}</a>
-          </TableCell>
-          <TableCell align="right">
-            <SmallButton
-              icon="edit"
-              onClick={() => {
-                //setEditPage(page);
-              }}
-            />
-          </TableCell>
-        </TableRow>
-      )
-    });
-    return result;
-  }
-
 
   //<YourSiteSettings />
-
+  if (windowWidth < 882){
+    return <ErrorMessages errors={["Page editor is only available in desktop mode"]} />;
+  }
   return (
     <AdminSiteWrapper config={props.config}>
-      <h1>Pages</h1>
       <ErrorMessages errors={errors} />
       <Grid container spacing={3}>
         <Grid item md={8} xs={12}>
-          <SiteNavigation links={links} pages={pages} refresh={loadData} select={(link, page) => {}} />
-          <DisplayBox headerIcon="link" headerText="Not Linked">
-            <table className="table">
-              {getUnlinkedPages() }
-            </table>
+          <DisplayBox headerText="Pages" headerIcon="article">
+            <p>Use the left navigation to edit pages and links.</p>
+          </DisplayBox>
+
+          <DisplayBox headerText="Reusable Blocks" headerIcon="smart_button" editContent={editBlockContent}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Type</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>{blocksUi}</TableBody>
+            </Table>
           </DisplayBox>
         </Grid>
         <Grid item md={4} xs={12}>
-          <NavItemEdit updatedFunction={loadData} link={undefined} page={undefined} links={links} />
+          {editBlock && (<BlockEdit block={editBlock}
+            updatedCallback={() => {
+              setEditBlock(null);
+              loadData();
+            }}
+          />
+          )}
         </Grid>
       </Grid>
 
