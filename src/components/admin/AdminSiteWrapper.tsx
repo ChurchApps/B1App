073e88892139f62f3b3ac3087f0e5cb540870042
Jeremy Zongker <jeremy@trilitech.com>
2024-07-18 12:46:47 -0500

    Combined link and page editors (#290)
    * Started UI updates to page admin nav
    
    * Updated PageEdit to edit both link and page
    
    * Separated site pagelink edit from mobile/video page edit
    
    * Add and delete page and links works
    
    * Completed drag and drop
    
    * Cleaned up /admin/site page

---
+++ src/components/admin/AdminSiteWrapper.tsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+import { ConfigurationInterface } from "@/helpers/ConfigHelper";
+import { AdminWrapper } from "./AdminWrapper";
+import { ApiHelper, LinkInterface, SmallButton } from "@churchapps/apphelper";
+import { PageInterface } from "@/helpers";
+import router from "next/router";
+import { SiteNavigation } from "./SiteNavigation";
+import { useWindowWidth } from "@react-hook/window-size";
+import Link from "next/link";
+import { AddPageModal } from "./site/AddPageModal";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DroppableWrapper } from "./DroppableWrapper";
+import { DraggableWrapper } from "./DraggableWrapper";
+
+interface Props {
+  config: ConfigurationInterface;
+  pageTitle?: string;
+  children: React.ReactNode;
+}
+
+export const AdminSiteWrapper: React.FC<Props> = (props) => {
+  const { isAuthenticated } = ApiHelper;
+  const [links, setLinks] = useState<LinkInterface[]>([]);
+  const [pages, setPages] = useState<PageInterface[]>([]);
+  const [errors, setErrors] = useState<string[]>([]);
+  const [addMode, setAddMode] = useState<string>("");
+  const windowWidth = useWindowWidth();
+
+  const loadData = () => {
+    if (!isAuthenticated) return;
+
+    ApiHelper.get("/pages", "ContentApi").then((_pages) => {
+      let filteredPages:PageInterface[] = [];
+      _pages.forEach((p:PageInterface) => {
+        if (!p.url?.startsWith("/stream/") && !p.url?.startsWith("/member/")) filteredPages.push(p);
+      });
+      setPages(filteredPages || [])
+    });
+
+    ApiHelper.get("/links?category=website", "ContentApi").then(data => { setLinks(data); });
+  };
+
+
+  useEffect(() => { if (!isAuthenticated) router.push("/login"); }, []);
+  useEffect(loadData, [isAuthenticated]);
+
+  const navigationHandler = (errorMessage: string, url: string) => {
+    let errors: string[] = [];
+
+    if (windowWidth > 882){
+      router.push(url);
+    } else {
+      errors.push(errorMessage);
+    }
+
+    if (errors.length > 0) {
+      setErrors(errors);
+      return;
+    }
+  }
+
+  const handleDrop = (index:number, parentId:string, page:PageInterface, link:LinkInterface) => {
+    if (parentId === "unlinked") {
+      //delete link
+      if (link) ApiHelper.delete("/links/" + link.id, "ContentApi").then(() => { loadData(); });
+    } else {
+      if (link) {
+        //move link
+        link.parentId = parentId;
+        if (link.parentId === "") link.parentId = null;
+        link.sort = index;
+        ApiHelper.post("/links", [link], "ContentApi").then(() => { loadData(); });
+      } else {
+        //add link
+        const newLink:LinkInterface = {id:"", churchId:page.churchId, category:"website", url:page.url, linkType:"url", linkData:"", icon:"", text:page.title, sort:index, parentId:parentId};
+        ApiHelper.post("/links", [newLink], "ContentApi").then(() => { loadData(); });
+      }
+    }
+
+  }
+
+  const getUnlinkedPages = () => {
+    const unlinkedPages = pages.filter(p => !links.find(l => l.url === p.url));
+    const result = unlinkedPages.map((page) =>
+
+    //const clickHandler = () => navigationHandler("Page editor is only accessible on desktop", "/admin/site/pages/" + page.id);
+
+      (
+        <tr key={page.id}>
+          <td>
+            <DraggableWrapper dndType="navItemPage" data={{page, link:null}}>
+              <Link href={"/admin/site/pages/preview/" + page.id}>{page.title}</Link>
+            </DraggableWrapper>
+          </td>
+        </tr>
+      )
+    );
+    return result;
+  }
+
+  const addLinkCallback = (page:PageInterface, link:LinkInterface) => {
+    loadData();
+    setAddMode("");
+    if (page) {
+      if (link) router.push("/admin/site/pages/preview/" + page.id + "?linkId=" + link.id);
+      else router.push("/admin/site/pages/preview/" + page.id);
+    }
+  }
+
+  return (
+    <AdminWrapper config={props.config}>
+      {(addMode!=="") && <AddPageModal updatedCallback={addLinkCallback} onDone={() => { setAddMode("") } } mode={addMode} />}
+
+      <Grid container spacing={3}>
+        <Grid item md={2} xs={12} style={{backgroundColor:"#FFF", minHeight:"100vh", marginTop:-7}}>
+          <DndProvider backend={HTML5Backend}>
+            <h2 style={{marginTop:0}}>Pages</h2>
+            <span style={{float:"right"}}>
+              <SmallButton icon="add" onClick={() => { setAddMode("navigation") }} />
+            </span>
+            <h3>Main Navigation</h3>
+            <SiteNavigation links={links} pages={pages} refresh={loadData} select={(link, page) => {}} handleDrop={handleDrop} />
+
+            <span style={{float:"right", paddingTop:15}}>
+              <SmallButton icon="add" onClick={() => { setAddMode("unlinked") }} />
+            </span>
+            <h3>Not Linked</h3>
+            <table className="table">
+              {getUnlinkedPages() }
+            </table>
+            <DroppableWrapper accept={["navItemPage"]} onDrop={(item) => {handleDrop(0, "unlinked", item.data.page, item.data.link)}} hideWhenInactive={true}><div style={{height:5}}></div></DroppableWrapper>
+          </DndProvider>
+        </Grid>
+        <Grid item md={10} xs={12}>
+          {props.children}
+        </Grid>
+      </Grid>
+
+    </AdminWrapper>
+  );
+};
---
+++ src/pages/[sdSlug]/admin/site/pages/preview/[id].tsx
@@ -0,0 +1,72 @@
+import {  ConfigHelper, GlobalStyleInterface, PageInterface, WrapperPageProps } from "@/helpers";
+import { ApiHelper, ChurchInterface, LinkInterface, SmallButton } from "@churchapps/apphelper";
+import React, { useEffect } from "react";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { AdminSiteWrapper } from "@/components/admin/AdminSiteWrapper";
+import { Grid } from "@mui/material";
+import { PageLinkEdit } from "@/components/admin/site/PageLinkEdit";
+import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
+
+interface Props extends WrapperPageProps {
+  pageData: any;
+  church: ChurchInterface,
+  churchSettings: any,
+  globalStyles: GlobalStyleInterface
+};
+
+export default function Preview(props: Props) {
+  const url = "/" + props.pageData.url;
+  const [showSettings, setShowSettings] = React.useState(false);
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const [link, setLink] = React.useState<LinkInterface>(null);
+
+  const loadData = () => {
+    const linkId = searchParams.get("linkId");
+    if (linkId) ApiHelper.get("/links/" + linkId, "ContentApi").then(data => { setLink(data); });
+  }
+
+  const handlePageUpdated = (page: PageInterface, link:LinkInterface) => {
+    setShowSettings(false);
+    if (!page) router.push("/admin/site/pages");
+    else router.refresh();
+  }
+
+  useEffect(loadData, [searchParams.get("linkId")]);
+
+  return <AdminSiteWrapper config={props.config}>
+    {showSettings && <PageLinkEdit link={link} page={props.pageData} updatedCallback={handlePageUpdated} onDone={() => setShowSettings(false)} />}
+    <div style={{marginLeft:-22, marginTop:-30, marginRight:-22}}>
+      <div style={{background:"#FFF", padding:15}}>
+        <Grid container>
+          <Grid item xs={3}>
+            <SmallButton icon="edit" text="Edit Content" href={"../" + props.pageData.id} />
+          </Grid>
+          <Grid item xs={6} style={{textAlign:"center"}}>
+            <b>{props.pageData.title}</b><br />
+          </Grid>
+          <Grid item xs={3} style={{textAlign:"right"}}>
+            <SmallButton icon="settings" text="Page Settings" onClick={() => setShowSettings(true)} />
+          </Grid>
+        </Grid>
+
+      </div>
+      <iframe src={url} style={{width:"100%", height:"100vh"}} />
+    </div>
+  </AdminSiteWrapper>
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths:any[] = [];
+  return { paths, fallback: "blocking", };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const config = await ConfigHelper.load(params.sdSlug.toString());
+  const church: ChurchInterface = await ApiHelper.getAnonymous("/churches/lookup?subDomain=" + params.sdSlug, "MembershipApi");
+  const churchSettings: any = await ApiHelper.getAnonymous("/settings/public/" + church.id, "MembershipApi");
+  const globalStyles: GlobalStyleInterface = await ApiHelper.getAnonymous("/globalStyles/church/" + church.id, "ContentApi");
+  const pageData: PageInterface = await ApiHelper.getAnonymous("/pages/" + church.id + "/tree?id=" + params.id, "ContentApi");
+  return { props: { config, churchSettings, globalStyles, pageData, church }, revalidate: 30 };
+};
