38554d9686bfa298b91243cb08b53e5384a01343
Himali Malvawala <himalimalvawala@gmail.com>
2024-08-05 23:55:59 +0530

    improve

--- src/components/admin/elements/PickColors.tsx
+++ src/components/admin/elements/PickColors.tsx
@@ -125,9 +125,11 @@ export function PickColors(props: Props) {
       result.push(<>
         <img src={props.background} style={{ maxHeight: 100, maxWidth: "100%", width: "auto" }} alt="background image" /><br />
         <Button variant="contained" onClick={() => setSelectPhotoField("photo")}>Select photo</Button>
-        <TextField fullWidth size="small" label="Background Opacity" name="backgroundOpacity" value={props?.backgroundOpacity || "0.55"} onChange={props.onChange}
-          type="number" sx={{ marginTop: 2 }} helperText={<>The opacity of black gradient layer on top of the image.<br />Max value: 1</>} FormHelperTextProps={{ sx: { marginLeft: 1 } }} InputProps={{ inputProps: { min: "0", max: "1", step: "1" } }}
-        />
+        {props?.onChange && (
+          <TextField fullWidth size="small" label="Background Opacity" name="backgroundOpacity" value={props?.backgroundOpacity || "0.55"} onChange={props.onChange}
+            type="number" sx={{ marginTop: 2 }} helperText={<>The opacity of black gradient layer on top of the image.<br />Max value: 1</>} FormHelperTextProps={{ sx: { marginLeft: 1 } }} InputProps={{ inputProps: { min: "0", max: "1", step: "1" } }}
+          />
+        )}
       </>)
     }
 
