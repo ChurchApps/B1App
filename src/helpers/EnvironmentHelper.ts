73dcf17190462ce807bd37d91fecd5f28c51503d
Jeremy Zongker <jeremy@zongker.net>
2024-08-12 16:20:09 -0500

    Fixed broken locale url

--- src/helpers/EnvironmentHelper.ts
+++ src/helpers/EnvironmentHelper.ts
@@ -29,7 +29,7 @@ export class EnvironmentHelper {
     if (typeof window !== "undefined") {
       baseUrl = window.location.origin;
     }
-    await Locale.init([`{baseUrl}/apphelper/locales/{{lng}}.json`])
+    await Locale.init([baseUrl + `/apphelper/locales/{{lng}}.json`])
   };
 
   static initDev = () => {
