232459db510ecb8621344f8f8397695126d5a7ec
Jeremy Zongker <jeremy@zongker.net>
2024-08-16 18:18:51 -0500

    Updated label to Check In

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -93,7 +93,7 @@ export const Wrapper: React.FC<Props> = props => {
 
     if (showWebsite) tabs.push(<NavItem key="/" url="/" label="Website" icon="home" router={router} />);
     if (showMyGroups) tabs.push(<NavItem key="/member" url="/member" label="My Groups" icon="group" router={router} selected={selectedTab === "groups"} />);
-    if (showCheckin) tabs.push(<NavItem key="/member/checkin" url="/member/checkin" label="Checkin" icon="check_box" router={router} selected={selectedTab === "checkin"} />)
+    if (showCheckin) tabs.push(<NavItem key="/member/checkin" url="/member/checkin" label="Check In" icon="check_box" router={router} selected={selectedTab === "checkin"} />)
     if (showDonations) tabs.push(<NavItem key="/member/donate" url="/member/donate" label="Donate" icon="volunteer_activism" router={router} selected={selectedTab === "donation"} />)
     if (showDirectory) tabs.push(<NavItem key="/member/directory" url="/member/directory" label="Member Directory" icon="groups" router={router} selected={selectedTab === "directory"} />);
     if (showPlans) tabs.push(<NavItem key="/member/plans" url="/member/plans" label="Plans" icon="event" router={router} selected={selectedTab === "plans"} />);
