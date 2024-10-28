a75e87595d601eda310d0ff47362a2d5d1b68195
Jeremy Zongker <jeremy@zongker.net>
2024-10-27 22:05:07 -0500

    Added meta data for pages

--- src/app/[sdSlug]/[pageSlug]/page.tsx
+++ src/app/[sdSlug]/[pageSlug]/page.tsx
@@ -1,7 +1,10 @@
 import { PageLayout, Theme } from "@/components";
 import { ApiHelper, ChurchInterface } from "@churchapps/apphelper";
-import { ConfigHelper, GlobalStyleInterface, PageInterface } from "@/helpers";
+import { ConfigHelper, EnvironmentHelper, GlobalStyleInterface, PageInterface } from "@/helpers";
 import { ConfigurationInterface } from "@/helpers/ConfigHelper";
+import { unstable_cache } from "next/cache";
+import { MetaHelper } from "@/helpers/MetaHelper";
+import { Metadata } from "next";
 
 // interface Props {
 //   params: {
@@ -10,19 +13,35 @@ import { ConfigurationInterface } from "@/helpers/ConfigHelper";
 //   };
 // }
 
-type Params = Promise<{ sdSlug: string;  pageSlug: string; }>
+type PageParams = Promise<{ sdSlug: string;  pageSlug: string; }>
 
-export default async function Home({ params }: { params: Params }) {
+const loadSharedData = (sdSlug:string, pageSlug:string) => {
+  const result = unstable_cache(loadData, ["/[sdSlug]", sdSlug], {tags:["all"]});
+  return result(sdSlug, pageSlug);
+}
 
-  const { sdSlug, pageSlug } = await params;
+export async function generateMetadata({params}: {params:PageParams}): Promise<Metadata> {
+  const { sdSlug, pageSlug } =  await params;
+  const props = await loadSharedData(sdSlug, pageSlug);
+  return MetaHelper.getMetaData(props.pageData.title + " - " + props.church.name, props.pageData.title, props.churchSettings.ogImage);
+}
 
-
+const loadData = async (sdSlug:string, pageSlug:string) => {
+  EnvironmentHelper.init();
   const church: ChurchInterface = await ApiHelper.getAnonymous("/churches/lookup?subDomain=" + sdSlug, "MembershipApi");
   const churchSettings: any = await ApiHelper.getAnonymous("/settings/public/" + church.id, "MembershipApi");
   const globalStyles: GlobalStyleInterface = await ApiHelper.getAnonymous("/globalStyles/church/" + church.id, "ContentApi");
   const navLinks: any = await ApiHelper.getAnonymous("/links/church/" + church.id + "?category=website", "ContentApi");
   const pageData: PageInterface = await ApiHelper.getAnonymous("/pages/" + church.id + "/tree?url=" + pageSlug, "ContentApi");
   const config: ConfigurationInterface = await ConfigHelper.load(church.subDomain);
+
+  return { pageData, church, churchSettings, navLinks, globalStyles, config }
+}
+
+export default async function Home({ params }: { params: PageParams }) {
+
+  const { sdSlug, pageSlug } = await params;
+  const { church, churchSettings, globalStyles, navLinks, pageData, config } = await loadSharedData(sdSlug, pageSlug);
 
   return (
     <>
