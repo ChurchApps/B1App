601d7b464dcfc39dfb2591df1e93bdf0e3c01d90
Jeremy Zongker <jeremy@zongker.net>
2024-10-30 18:31:34 -0500

    Fixed jwt login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,69 +1,3 @@
-// "use client";
-
-// import { useContext, useEffect } from "react"
-// import { useRouter } from "next/navigation";
-// import { useCookies } from "react-cookie";
-// import { PaperProps } from "@mui/material"
-// import { Layout } from "@/components";
-// import { LoginPage, ApiHelper, UserHelper } from "@churchapps/apphelper";
-// import UserContext from "@/context/UserContext"
-// import { PersonHelper } from "@/helpers";
-// import { useSearchParams } from "next/navigation";
-
-// interface Props {
-//   showLogo?: boolean;
-//   redirectAfterLogin?: boolean;
-//   loginContainerCssProps?: PaperProps
-//   keyName?: string;
-// }
-
-// export function Login({ showLogo, redirectAfterLogin = true, loginContainerCssProps, keyName }: Props) {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
-//   let returnUrl: string | null = searchParams.get("returnUrl");
-//   const [cookies] = useCookies();
-//   const context = useContext(UserContext)
-
-//   if (!returnUrl) returnUrl = "/member"
-
-
-//   useEffect(() => {
-//     if (ApiHelper.isAuthenticated && UserHelper.currentUserChurch?.church) {
-//       PersonHelper.person = context.person;
-//       // Perform navigation after component has rendered
-//       router.push(`${returnUrl}`);
-//     }
-//   }, [context.person, returnUrl, router]);
-//   // if (ApiHelper.isAuthenticated && UserHelper.currentUserChurch?.church) {
-//   //   PersonHelper.person = context.person;
-//   //   router.push(`${returnUrl}`);
-//   // }
-
-//   const appUrl = process.browser ? window.location.href : "";
-//   let jwt: string = "",
-//   auth: string | null = searchParams.get("auth");
-//   if (!ApiHelper.isAuthenticated) {
-//     let search = new URLSearchParams(process.browser ? window.location.search : "");
-//     jwt = search.get("jwt") || cookies.jwt;
-//   }
-
-//   return (
-//     <Layout withoutNavbar withoutFooter>
-//       <LoginPage
-//         auth={auth}
-//         context={context}
-//         jwt={jwt}
-//         appName="B1"
-//         showLogo={showLogo}
-//         loginContainerCssProps={loginContainerCssProps}
-//         keyName={keyName}
-//         returnUrl={returnUrl}
-//       />
-//     </Layout>
-//   );
-// }
-
-
 "use client";
 
 import { useContext, useEffect, useState } from "react";
@@ -106,10 +40,8 @@ export function Login({ showLogo, redirectAfterLogin = true, loginContainerCssPr
   useEffect(() => {
     // Client-side only logic
     if (typeof window !== "undefined") {
-      const search = new URLSearchParams(window.location.search);
-      const auth = searchParams.get("auth");
-      const jwtFromUrl = search.get("jwt") || cookies.jwt;
-
+      //const search = new URLSearchParams(window.location.search);
+      const jwtFromUrl = searchParams.get("jwt") || cookies.jwt;
       setJwt(jwtFromUrl || "");
     }
   }, [cookies.jwt, searchParams]);
