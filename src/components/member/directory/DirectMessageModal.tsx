dcec1c2f5be8a67bc91a0c5aa66e9a9cfd46b1e2
Jeremy Zongker <jeremy@zongker.net>
2024-10-30 16:35:18 -0500

    Added direct messages to member profile page

---
+++ src/components/member/directory/DirectMessageModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Dialog, DialogContent } from "@mui/material";
+import React from "react";
+
+import { PersonInterface, PrivateMessageInterface, UserContextInterface } from "@churchapps/helpers";
+import { NewPrivateMessage, PrivateMessageDetails } from "@churchapps/apphelper";
+
+interface Props {
+  context: UserContextInterface;
+  onBack: () => void
+  person?: PersonInterface
+}
+
+export const DirectMessageModal: React.FC<Props> = (props) => {
+  const [selectedMessage, setSelectedMessage] = React.useState<PrivateMessageInterface>(null);
+  let a= 0;
+
+  const handleMessageSelect = (pm: PrivateMessageInterface) => {
+    setSelectedMessage(pm);
+  }
+
+  return (
+    <Dialog open={true} onClose={props.onBack} fullWidth scroll="body">
+      <DialogContent>
+        {!selectedMessage && <NewPrivateMessage context={props.context} selectedPerson={props.person} onSelectMessage={handleMessageSelect} onBack={props.onBack} />}
+        {selectedMessage && <PrivateMessageDetails privateMessage={selectedMessage} context={props.context} onBack={props.onBack} refreshKey={1} />}
+      </DialogContent>
+    </Dialog>)
+
+}
