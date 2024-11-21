9157f300d013e9c5a06fc5079216bdaee5c6cc40
KZongker <168384631+KZongker@users.noreply.github.com>
2024-11-21 17:53:32 -0600

    Form Editing Margin Fix (#329)

--- src/components/admin/ContentEditor/ContentEditor.tsx
+++ src/components/admin/ContentEditor/ContentEditor.tsx
@@ -3,7 +3,7 @@ import { CSSProperties, useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 import { Container, Dialog, Grid, Icon, ThemeProvider, ToggleButton, ToggleButtonGroup, Tooltip, createTheme } from "@mui/material";
 import { useWindowWidth } from "@react-hook/window-size";
-import {  BlockInterface, ElementInterface, GlobalStyleInterface, PageInterface, SectionInterface, WrapperPageProps } from "@/helpers";
+import { BlockInterface, ElementInterface, GlobalStyleInterface, PageInterface, SectionInterface, WrapperPageProps } from "@/helpers";
 import { Theme } from "@/components";
 import { ApiHelper, ArrayHelper, ChurchInterface, UserHelper, Permissions, SmallButton, DisplayBox } from "@churchapps/apphelper";
 import { Section } from "@/components/Section";
@@ -24,7 +24,7 @@ interface Props extends WrapperPageProps {
   church: ChurchInterface,
   churchSettings: any,
   globalStyles: GlobalStyleInterface,
-  loadData: (id:string) => Promise<any>,
+  loadData: (id: string) => Promise<any>,
   pageId?: string,
   blockId?: string
   onDone?: (url?: string) => void
@@ -47,14 +47,14 @@ export default function ContentEditor(props: Props) {
   let elementOnlyMode = false;
   if (props.blockId && container?.sections?.length===1 && container?.sections[0]?.id==="") elementOnlyMode = true;
 
-  const zones:any = {
+  const zones: any = {
     cleanCentered: ["main"],
     embed: ["main"],
     headerFooter: ["main", "footer"],
   }
 
   useEffect(() => {
-    if(!UserHelper.checkAccess(Permissions.contentApi.content.edit)){
+    if (!UserHelper.checkAccess(Permissions.contentApi.content.edit)) {
       redirect("/admin/site");
     }
   }, []);
@@ -78,7 +78,7 @@ export default function ContentEditor(props: Props) {
 
   //page editor only available for desktop
   useEffect(() => {
-    if (windowWidth < 883){
+    if (windowWidth < 883) {
       redirect("/admin/site");
     }
   }, [windowWidth]);
@@ -88,12 +88,12 @@ export default function ContentEditor(props: Props) {
       const section: SectionInterface = data.data;
       section.sort = sort;
       section.zone = zone;
-      section.pageId = (zone==="footer") ? null : props.pageId;
+      section.pageId = (zone === "footer") ? null : props.pageId;
       ApiHelper.post("/sections", [section], "ContentApi").then(() => { loadData() });
     }
     else {
-      const sec = { sort, background: "#FFF", textColor: "dark", pageId: props.pageId, blockId:props.blockId, targetBlockId: data.blockId, zone: zone }
-      if (sec.zone==="footer") sec.pageId = null;
+      const sec = { sort, background: "#FFF", textColor: "dark", pageId: props.pageId, blockId: props.blockId, targetBlockId: data.blockId, zone: zone }
+      if (sec.zone === "footer") sec.pageId = null;
       setEditSection(sec);
     }
   }
@@ -107,15 +107,14 @@ export default function ContentEditor(props: Props) {
   const getSections = (zone: string) => {
     const result: JSX.Element[] = []
     result.push(getAddSection(0, zone));
-    const sections = (zone==="block") ? container?.sections : ArrayHelper.getAll(container?.sections, "zone", zone);
+    const sections = (zone === "block") ? container?.sections : ArrayHelper.getAll(container?.sections, "zone", zone);
     sections?.forEach(section => {
       if (section.targetBlockId) result.push(<SectionBlock key={section.id} section={section} churchSettings={props.config.appearance} onEdit={handleSectionEdit} onMove={() => { loadData() }} />)
       else result.push(<Section key={section.id} section={section} churchSettings={props.config.appearance} onEdit={handleSectionEdit} onMove={() => { loadData() }} church={props.church} />)
       result.push(getAddSection(section.sort + 0.1, zone));
     });
 
-    if (sections.length===0)
-    {
+    if (sections.length === 0) {
       result.push(<Container><p>Add a section to get started</p></Container>)
     }
     return result;
@@ -130,7 +129,7 @@ export default function ContentEditor(props: Props) {
 
   if (typeof window !== "undefined") {
     const editorBar = document.getElementById("editorBar");
-    if(window.innerWidth > 900){
+    if (window.innerWidth > 900) {
       if (window?.innerHeight) {
         //const editorBarOffset = (editorBarHeight > window.innerHeight) ? (editorBarHeight - window.innerHeight) : 0;
         //const bottomMargin = editorBarOffset === 0 ? 0 : 50;
@@ -147,7 +146,7 @@ export default function ContentEditor(props: Props) {
       const page = container as PageInterface;
       if (page.layout === "embed") {
         if (page.url.includes("/member")) url = '/admin';
-        else if (page.url.includes("/stream")) url='/admin/video/settings';
+        else if (page.url.includes("/stream")) url = '/admin/video/settings';
       }
     }
     props.onDone(url);
@@ -155,7 +154,7 @@ export default function ContentEditor(props: Props) {
 
   /*Todo: affix the sidebar with CSS instead*/
   useEffect(() => {
-    const onScroll = (e:any) => { setScrollTop(e.target.documentElement.scrollTop); };
+    const onScroll = (e: any) => { setScrollTop(e.target.documentElement.scrollTop); };
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
   }, [scrollTop]);
@@ -178,33 +177,42 @@ export default function ContentEditor(props: Props) {
 
   const getTheme = () => {
     //force mobile
-    if (deviceType==="mobile") return createTheme({
+    if (deviceType === "mobile") return createTheme({
       breakpoints: {
         values: { xs: 0, sm: 2000, md: 3000, lg: 4000, xl: 5000 },
       },
+      components: {
+        MuiTextField: { defaultProps: { margin: "normal" } },
+        MuiFormControl: { defaultProps: { margin: "normal" } }
+      }
     });
-    else return createTheme();
+    else return createTheme({
+      components: {
+        MuiTextField: { defaultProps: { margin: "normal" } },
+        MuiFormControl: { defaultProps: { margin: "normal" } }
+      }
+    });
   }
 
   //<div style={{ height: "31px" }}>{getAddSection(sections[sections.length - 1]?.sort + 0.1, keyName, "Drop at the bottom of page")}</div>
-  const getZoneBox = (sections:SectionInterface[], name:string, keyName:string) => <div key={"zone-" + keyName} style={{minHeight:100}}>
-    <div style={{position:"absolute", backgroundColor:"#FFF", zIndex:100, padding:10, border:"1px solid #999", opacity:0.5  }}>Zone: {keyName}</div>
-    <div style={{minHeight:100}}>
+  const getZoneBox = (sections: SectionInterface[], name: string, keyName: string) => <div key={"zone-" + keyName} style={{ minHeight: 100 }}>
+    <div style={{ position: "absolute", backgroundColor: "#FFF", zIndex: 100, padding: 10, border: "1px solid #999", opacity: 0.5 }}>Zone: {keyName}</div>
+    <div style={{ minHeight: 100 }}>
       <>
-        <div className="page" style={(deviceType==="mobile" ? {width:400, marginLeft:"auto", marginRight:"auto"} : {})}>
+        <div className="page" style={(deviceType === "mobile" ? { width: 400, marginLeft: "auto", marginRight: "auto" } : {})}>
           {getSections(keyName)}
         </div>
       </>
     </div>
-    <div style={{ height: "31px"}}></div>
+    <div style={{ height: "31px" }}></div>
   </div>
 
   const getZoneBoxes = () => {
-    let result:any[] = [];
+    let result: any[] = [];
     let idx = 0;
     if (props.pageId) {
       const page = container as PageInterface;
-      if (page?.layout==="headerFooter") result.push(<Header church={props.church || {}} churchSettings={props.churchSettings} navLinks={navLinks} overlayContent={page?.url === "/"} sections={[]} globalStyles={props.globalStyles} editMode />)
+      if (page?.layout === "headerFooter") result.push(<Header church={props.church || {}} churchSettings={props.churchSettings} navLinks={navLinks} overlayContent={page?.url === "/"} sections={[]} globalStyles={props.globalStyles} editMode />)
       zones[page?.layout]?.forEach((z: string) => {
         const sections = ArrayHelper.getAll(page?.sections, "zone", z);
         const name = z.substring(0, 1).toUpperCase() + z.substring(1, z.length);
@@ -219,7 +227,7 @@ export default function ContentEditor(props: Props) {
   }
 
   const getHelp = () => (
-    <Dialog open={true} onClose={() => {setShowHelp(false)}} fullWidth maxWidth="sm">
+    <Dialog open={true} onClose={() => { setShowHelp(false) }} fullWidth maxWidth="sm">
       <DisplayBox id="dialogForm" headerIcon="help" headerText="Help">
         <p>Use the plus icon in the corner to add new sections and elements to a page.  All elements must go within a section.</p>
         <p>Doubleclick any section or element to edit or remove it.</p>
@@ -234,28 +242,28 @@ export default function ContentEditor(props: Props) {
     <Theme appearance={props.churchSettings} globalStyles={props.globalStyles} />
     <Helmet><style>{StyleHelper.getCss(container?.sections || [], deviceType)}</style></Helmet>
 
-    <div style={{backgroundColor:"#FFF", position:"sticky", top:0, width:"100%", zIndex:1000, boxShadow:"0px 2px 2px black", marginBottom:10}}>
+    <div style={{ backgroundColor: "#FFF", position: "sticky", top: 0, width: "100%", zIndex: 1000, boxShadow: "0px 2px 2px black", marginBottom: 10 }}>
       <Grid container spacing={2}>
-        <Grid item xs={4} style={{paddingLeft:40, paddingTop:8}}>
+        <Grid item xs={4} style={{ paddingLeft: 40, paddingTop: 8 }}>
           <SmallButton icon={"done"} text="Done" onClick={handleDone} />
         </Grid>
-        <Grid item xs={4} style={{textAlign:"center"}}>
+        <Grid item xs={4} style={{ textAlign: "center" }}>
           <b>
             {props.pageId && "Page: " + (container as PageInterface)?.title}
             {props.blockId && "Block: " + (container as BlockInterface)?.name}
           </b>
         </Grid>
-        <Grid item xs={4} style={{textAlign:"right", paddingTop:5, paddingBottom:5, paddingRight:15}}>
-          <div style={{float:"right", display:"flex", backgroundColor:"#1976d2" }}>
+        <Grid item xs={4} style={{ textAlign: "right", paddingTop: 5, paddingBottom: 5, paddingRight: 15 }}>
+          <div style={{ float: "right", display: "flex", backgroundColor: "#1976d2" }}>
             <ToggleButtonGroup value={showHelp.toString()} exclusive size="small">
-              <ToggleButton value="true" onClick={() => setShowHelp(!showHelp)} style={{borderRight:"1px solid #FFF", color:"#FFF"}}><Tooltip title="Help" placement="top"><Icon>help</Icon></Tooltip></ToggleButton>
+              <ToggleButton value="true" onClick={() => setShowHelp(!showHelp)} style={{ borderRight: "1px solid #FFF", color: "#FFF" }}><Tooltip title="Help" placement="top"><Icon>help</Icon></Tooltip></ToggleButton>
             </ToggleButtonGroup>
             <ToggleButtonGroup value={showAdd.toString()} exclusive size="small">
-              <ToggleButton value="true" onClick={() => setShowAdd(!showAdd)} style={{borderRight:"1px solid #FFF", color:"#FFF"}}><Tooltip title="Add Content" placement="top"><Icon>add</Icon></Tooltip></ToggleButton>
+              <ToggleButton value="true" onClick={() => setShowAdd(!showAdd)} style={{ borderRight: "1px solid #FFF", color: "#FFF" }}><Tooltip title="Add Content" placement="top"><Icon>add</Icon></Tooltip></ToggleButton>
             </ToggleButtonGroup>
-            <ToggleButtonGroup size="small" value={deviceType} exclusive onChange={(e, newDeviceType) => { if (newDeviceType!==null) setDeviceType(newDeviceType) }}>
-              {deviceType==="desktop" && <ToggleButton size="small" value="mobile" style={{color:"#FFF"}}><Tooltip title="Desktop" placement="top"><Icon>computer</Icon></Tooltip></ToggleButton>}
-              {deviceType==="mobile" && <ToggleButton size="small" value="desktop" style={{color:"#FFF"}}><Tooltip title="Mobile" placement="top"><Icon>smartphone</Icon></Tooltip></ToggleButton>}
+            <ToggleButtonGroup size="small" value={deviceType} exclusive onChange={(e, newDeviceType) => { if (newDeviceType !== null) setDeviceType(newDeviceType) }}>
+              {deviceType === "desktop" && <ToggleButton size="small" value="mobile" style={{ color: "#FFF" }}><Tooltip title="Desktop" placement="top"><Icon>computer</Icon></Tooltip></ToggleButton>}
+              {deviceType === "mobile" && <ToggleButton size="small" value="desktop" style={{ color: "#FFF" }}><Tooltip title="Mobile" placement="top"><Icon>smartphone</Icon></Tooltip></ToggleButton>}
             </ToggleButtonGroup>
           </div>
         </Grid>
@@ -271,16 +279,16 @@ export default function ContentEditor(props: Props) {
       {editSection && <SectionEdit section={editSection} updatedCallback={() => { setEditSection(null); loadData(); }} globalStyles={props.globalStyles} />}
 
       <div>
-        {scrollTop>150
-          && <div style={{position:"fixed", bottom:30, zIndex:1000, width:500, marginLeft:300}}>
-            <DroppableScroll key={"scrollDown"} text={"Scroll Down"} direction="down"  />
+        {scrollTop > 150
+          && <div style={{ position: "fixed", bottom: 30, zIndex: 1000, width: 500, marginLeft: 300 }}>
+            <DroppableScroll key={"scrollDown"} text={"Scroll Down"} direction="down" />
           </div>}
-        {scrollTop>150 && <div style={{position:"fixed", top: 50, zIndex:1000, width:500, marginLeft:300}}>
+        {scrollTop > 150 && <div style={{ position: "fixed", top: 50, zIndex: 1000, width: 500, marginLeft: 300 }}>
           <DroppableScroll key={"scrollUp"} text={"Scroll Up"} direction="up" />
-        </div> }
+        </div>}
 
 
-        <h1 style={{marginTop:-50}}>Edit Page</h1>
+        <h1 style={{ marginTop: -50 }}>Edit Page</h1>
         <ThemeProvider theme={getTheme()}>
           {getZoneBoxes()}
         </ThemeProvider>
