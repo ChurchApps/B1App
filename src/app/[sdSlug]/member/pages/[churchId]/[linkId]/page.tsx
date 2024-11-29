c59149076eb77775e3fd79a4ecf8b2a8bbcc1236
Himali Malvawala <himalimalvawala@gmail.com>
2024-11-29 14:56:57 +0530

    fix build

--- src/app/[sdSlug]/member/pages/[churchId]/[linkId]/page.tsx
+++ src/app/[sdSlug]/member/pages/[churchId]/[linkId]/page.tsx
@@ -1,12 +1,13 @@
 import { ConfigHelper, EnvironmentHelper } from "@/helpers";
 import { PagesClient } from "./PagesClient";
 
-type Params = Promise<{ sdSlug: string;  churchId: string; linkId: string; url?: string }>;
+type Params = Promise<{ sdSlug: string; churchId: string; linkId: string }>;
+type SearchParams = Promise<{ url: string }>;
 
-export default async function Pages({ params, searchParams }: {params:Params, searchParams: {url:string}}) {
+export default async function Pages({ params, searchParams }: { params: Params, searchParams: SearchParams }) {
   await EnvironmentHelper.initServerSide();
-  const {sdSlug, churchId, linkId}= await params
-  const {url} = searchParams;
+  const { sdSlug, churchId, linkId } = await params;
+  const { url } = await searchParams;
   const config = await ConfigHelper.load(sdSlug.toString());
 
   return (
