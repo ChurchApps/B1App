3f2cb3abf1063975ea6b2e1481d5159f271c3f6d
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2024-12-06 14:38:28 +0530

    add: donations video link to the tab (#336)

--- src/app/[sdSlug]/member/donate/DonateClient.tsx
+++ src/app/[sdSlug]/member/donate/DonateClient.tsx
@@ -47,11 +47,15 @@ export function DonateClient(props: WrapperPageProps) {
               <Grid item md={4} xs={12}>
                 <Typography
                   component="h3"
-                  sx={{ textAlign: "center", fontSize: "28px", fontWeight: 500, lineHeight: 1.2, margin: "0 0 8px 0" }}
+                  sx={{ textAlign: "center", fontSize: "30px", fontWeight: 500, lineHeight: 1.2, margin: "0 0 8px 0" }}
                 >
                 Manage Donations
                 </Typography>
-                <p style={{ marginTop: 0 }}>Please login to manage donations</p>
+                <a href="https://support.churchapps.org/b1/portal/donations/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "underline", textUnderlineOffset: 2, display: "flex", justifyContent: "center", alignItems: "center", fontSize: "15px" }}>
+                  <p>Instructions for ACH Payment</p>
+                  <Icon sx={{ fontSize: "18px !important", marginLeft: 0.5 }}>open_in_new</Icon>
+                </a>
+                <p style={{ textAlign: "center", fontSize: "12.5px", marginBottom: 3, fontStyle: "italic" }}>Please login to manage donations</p>
                 <Link href="/login/?returnUrl=/member/donate">
                   <Button sx={{ fontSize: "16px", textTransform: "capitalize" }} fullWidth variant="contained">
                   Login
