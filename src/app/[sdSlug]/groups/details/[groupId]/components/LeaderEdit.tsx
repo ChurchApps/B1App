b5d751399ae00f3b8665cb66b770fb79dfe30763
Kat Zongker <katrina@zongker.net>
2025-01-16 22:28:25 -0600

    LeaderEditing

---
+++ src/app/[sdSlug]/groups/details/[groupId]/components/LeaderEdit.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { ConfigurationInterface } from "@/helpers/ConfigHelper";
+import { ApiHelper, EventInterface, GroupInterface, GroupMemberInterface, InputBox } from "@churchapps/apphelper";
+import { Button, SelectChangeEvent, TextField } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import { useEffect, useState } from "react";
+
+interface Props {
+    config: ConfigurationInterface
+    group: GroupInterface;
+    onChange: (group: GroupInterface) => void;
+}
+
+export function LeaderEdit(props: Props) {
+
+    const [formEdits, setFormEdits] = useState<GroupInterface>(props.group);
+    let [hidden, setHidden] = useState("none");
+
+    useEffect(() => {
+        setFormEdits(props.group);
+        console.log("group is", props.group);
+    }, [props.group])
+
+    const hideForm = () => {
+        if (hidden === "none") {
+            setHidden("block");
+            console.log("showing")
+        } else {
+            setHidden("none");
+            console.log("hiding")
+        }
+        return;
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string>) => {
+        const val = e.target.value;
+        const fe = { ...formEdits }
+        switch (e.target.name) {
+            case "catergoryName": fe.categoryName = val; break;
+            case "name": fe.name = val; break;
+            case "meetingTime": fe.meetingTime = val; break;
+            case "meetingLocation": fe.meetingLocation = val; break;
+            case "about": fe.about = val; break;
+        }
+        setFormEdits(fe);
+        console.log(fe);
+    }
+
+    const handleSubmit = async () => {
+        ApiHelper.post("/groups", [formEdits], "MembershipApi").then((groups: GroupInterface[]) => {
+            hideForm();
+            props.onChange(groups[0]);
+        });
+    }
+
+    return <>
+        <div style={{ textAlign: "right", marginTop: "20px" }}>
+            <Button onClick={hideForm}><EditIcon /></Button>
+        </div>
+
+        <div>
+            <form style={{ display: hidden }}>
+                <InputBox saveFunction={handleSubmit} cancelFunction={hideForm}>
+                    <TextField fullWidth label="Category Name" name="categoryName" value={formEdits.categoryName || ""} onChange={handleChange} />
+                    <TextField fullWidth label="Group Name" name="name" value={formEdits.name || ""} onChange={handleChange} />
+                    <TextField fullWidth label="Meeting Time" name="meetingTime" value={formEdits.meetingTime || ""} onChange={handleChange} />
+                    <TextField fullWidth label="Meeting Location" name="meetingLocation" value={formEdits.meetingLocation || ""} onChange={handleChange} />
+                    <TextField fullWidth label="Description" name="about" value={formEdits.about || ""} onChange={handleChange} multiline />
+                </InputBox>
+            </form>
+        </div>
+    </>
+}
