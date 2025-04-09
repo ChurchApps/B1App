7d6cf693aa5969ab726d28ea6235d35f03edc5f1
Jeremy Zongker <jeremy@trilitech.com>
2025-04-08 21:43:23 -0500

    Service order (#360)
    * Added Order of Service component
    
    * Added song details
    
    * Updated download

--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -211,3 +211,69 @@ export interface PageLink {
   children?: PageLink[];
   expanded?: boolean;
 }
+
+
+
+
+/* Temp for songs.  Move to Helpers when stable */
+
+
+export interface SongInterface {
+  id?: string,
+  name?: string,
+  dateAdded: Date
+}
+
+export interface SongDetailInterface {
+  id?: string,
+  praiseChartsId?: string,
+  title?: string,
+  artist?: string,
+  album?: string,
+  language?: string,
+  thumbnail?: string,
+  releaseDate?: Date,
+  bpm?: number,
+  keySignature?: string,
+  seconds: number,
+  meter?: string,
+  tones?: string,
+}
+
+export interface SongDetailLinkInterface {
+  id?: string,
+  songDetailId?: string,
+  service?: string,
+  serviceKey?: string,
+  url?: string,
+}
+
+export interface ArrangementInterface {
+  id?: string,
+  songId?: string,
+  songDetailId?: string,
+  name?: string,
+  lyrics?: string
+}
+
+
+export interface ArrangementKeyInterface {
+  id?: string,
+  arrangementId?: string,
+  keySignature?: string,
+  shortDescription?: string
+}
+
+export interface PlanItemInterface {
+  id?: string,
+  planId?: string,
+  parentId?: string,
+  sort?: number,
+  itemType?: string,
+  relatedId?: string,
+  label?: string,
+  description?: string,
+  seconds?: number
+
+  children?: PlanItemInterface[]
+}
