703c41e02318fb2c0b007c355f0c5366446b99b9
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2025-05-29 16:45:21 +0530

    improve: donate link feature (#368)
    * improve: donate link feature
    
    * fix: build

--- src/components/elementTypes/DonateLinkElement.tsx
+++ src/components/elementTypes/DonateLinkElement.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { ElementInterface } from "@/helpers";
 
 interface Props {
@@ -6,24 +6,36 @@ interface Props {
 }
 
 export function DonateLinkElement({ element }: Props) {
+  const amounts = (element.answers?.amounts && element.answers.amounts.length > 0) ? JSON.parse(element.answers.amounts) : [];
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: element.answers?.alignment || "left",
-        margin: "15px 0",
-      }}
-    >
-      <Button
-        variant="contained"
-        size="small"
-        startIcon={<Icon>volunteer_activism</Icon>}
-        href={`${element.answers?.url}?fundId=${element.answers?.fundId}&amounts=${element.answers?.amounts}`}
-        target="_blank"
-      >
-        {element.answers?.text || "Donate Now"}
-      </Button>
+    <div id={"el-" + element.id}>
+      <Box sx={{ backgroundColor: "white", padding: "20px", borderRadius: "15px", marginBottom: "15px" }}>
+        <h4 style={{ marginTop: 10, marginBottom: 15 }}>
+          {element.answers?.text?.toUpperCase() || "DONATE NOW"}
+        </h4>
+        {amounts?.map((a: number, index: number) => (
+          <Button
+            variant="outlined"
+            size="small"
+            key={index}
+            sx={{ minWidth: "70px", marginRight: "10px", marginTop: "5px", borderWidth: "2px", borderRadius: "10px", fontWeight: "bold" }}
+            href={`${element.answers?.url}?amount=${a}&fundId=${element.answers?.fundId}`}
+            target="_blank"
+          >
+            $ {a}
+          </Button>
+        ))}
+        <Button
+          variant="outlined"
+          size="small"
+          sx={{ marginTop: "5px", borderWidth: "2px", borderRadius: "10px", fontWeight: "bold" }}
+          href={`${element.answers?.url}?fundId=${element.answers?.fundId}`}
+          target="_blank"
+        >
+          Other
+        </Button>
+      </Box>
     </div>
   );
 }
