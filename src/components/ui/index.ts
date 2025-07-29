57181550eeae7ab221a1173107bc6dd776cdd58f
Kat Zongker <katrina@zongker.net>
2025-07-29 16:19:28 -0500

    avatarImport

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,4 +3,4 @@ export { StatusChip } from "./StatusChip";
 export { EmptyState } from "./EmptyState";
 export { CardWithHeader } from "./CardWithHeader";
 export { LoadingButton } from "./LoadingButton";
-export { PersonAvatar } from "./PersonAvatar";
+export { PersonAvatar } from "@churchapps/apphelper";
