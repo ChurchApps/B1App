1e0683a3267092072e90e3df5d0014fd486a48fa
Jeremy Zongker <jeremy@zongker.net>
2025-12-15 14:02:27 -0600

    Handle Stream element within B1App

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,1 +1,36 @@
-export { Element } from "@churchapps/apphelper-website";
+"use client";
+
+import React from "react";
+import { Element as AppHelperElement, ElementBlock } from "@churchapps/apphelper-website";
+import type { ChurchInterface } from "@churchapps/helpers";
+import { ElementInterface, SectionInterface } from "@/helpers";
+import { LiveStream } from "./video/LiveStream";
+
+interface Props {
+  element: ElementInterface;
+  church?: ChurchInterface;
+  churchSettings: any;
+  textColor: string;
+  onEdit?: (section: SectionInterface | null, element: ElementInterface) => void;
+  onMove?: () => void;
+  parentId?: string;
+}
+
+const StreamElement: React.FC<{ element: ElementInterface; churchSettings: any; church: ChurchInterface; editMode: boolean }> = (props) => {
+  const mode = props.element.answers?.mode;
+  const includeInteraction = mode !== "video";
+
+  let offlineContent: React.ReactElement | undefined = undefined;
+  if (props.element.answers?.offlineContent === "hide") offlineContent = (props.editMode) ? (<>Offline Video Placeholder</>) : (<></>);
+  else if (props.element.answers?.offlineContent === "block") offlineContent = <ElementBlock key={props.element.id} element={props.element as ElementInterface} churchSettings={props.churchSettings} textColor={"#333333"} />;
+
+  return <LiveStream includeHeader={false} includeInteraction={includeInteraction} keyName={props.church.subDomain || ""} appearance={props.churchSettings} offlineContent={offlineContent} />;
+};
+
+export const Element: React.FC<Props> = (props) => {
+  if (props.element.elementType === "stream" && props.church) {
+    return <StreamElement element={props.element} churchSettings={props.churchSettings} church={props.church} editMode={!!props.onEdit} />;
+  }
+
+  return <AppHelperElement {...props} />;
+};
